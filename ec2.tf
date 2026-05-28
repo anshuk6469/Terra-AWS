ad76311d89d5ce4835b85ee6e1f6300795ef6524
Deepanshu Kumar <anshuk6469@gmail.com>
2026-05-28 16:14:00 +0530

    ec2.tf modified

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "linux" {
   ami           = "ami-091138d0f0d41ff90"
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   count = 2
   tags = {
     Name = "Linux-web${count.index}"
